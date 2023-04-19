208bc651b3a192de221e0f5ebd901b5d12f41696
Aaron Brooker <aaronbrooker2110@gmail.com>
2023-04-19 20:59:29 +0100

    Complete first version of Etch-a-sketch

---
+++ script.js
@@ -0,0 +1,56 @@
+const resetButton = document.getElementById("reset-board");
+const changeButton = document.getElementById("change-resolution");
+const sketchBoard = document.getElementById("sketch-board");
+console.log(resetButton);
+console.log(sketchBoard);
+
+resetButton.addEventListener('click', resetBoard)
+changeButton.addEventListener('click', changeResolution)
+
+let gridResolution = 16;
+resetBoard();
+
+function resetBoard() {
+    removeGrid();
+    newGrid(gridResolution);
+    console.log("Board reset completed");
+};
+
+function changeResolution() {
+    let newGridResolution = Math.floor(prompt("Enter a number between 10 and 100"));
+    if (newGridResolution < 10 || newGridResolution > 100) {
+        alert("This number is invalid. Please try again with a number between 10 and 100.");
+        console.log("Resolution unchanged: "+gridResolution);
+    } else {
+        gridResolution = newGridResolution;
+        console.log("New resolution accepted: "+gridResolution);
+        resetBoard();
+    };
+};
+
+function removeGrid() {
+    while (sketchBoard.firstChild != null) {
+        sketchBoard.removeChild(sketchBoard.firstChild);
+    }
+    console.log("Grid cleared");
+};
+
+function newGrid(resolution) {
+    for (let i = 0; i<resolution; i++) {
+        const newRow = document.createElement('div');
+        newRow.classList.add('grid-row');
+        for (let j = 0; j<resolution; j++) {
+            const newCell = document.createElement('div');
+            newCell.classList.add('grid-cell');
+            newCell.addEventListener('mouseenter',hoverEffect);
+            newRow.appendChild(newCell);
+        };    
+        sketchBoard.appendChild(newRow);
+    };
+    console.log("New grid generated"); 
+};
+
+function hoverEffect() {
+    this.classList.add('hovered-cell');
+    console.log("Hover effect triggered");
+};
